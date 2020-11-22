64fda1575de867993575e59b68b25bad5e2a904b
Raz <raz.bne@gmail.com>
2020-11-22 23:27:54 +0200

    create new subspace

--- controllers/subspace_controller.go
+++ controllers/subspace_controller.go
@@ -18,13 +18,16 @@ package controllers
 
 import (
 	"context"
+	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/go-logr/logr"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	dana794v1 "dana.794/api/v1"
+	v1 "dana.794/api/v1"
+	corev1 "k8s.io/api/core/v1"
 )
 
 // SubspaceReconciler reconciles a Subspace object
@@ -38,16 +41,61 @@ type SubspaceReconciler struct {
 // +kubebuilder:rbac:groups=dana.794.dana.794,resources=subspaces/status,verbs=get;update;patch
 
 func (r *SubspaceReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
+	ctx := context.Background()
 	_ = r.Log.WithValues("subspace", req.NamespacedName)
 
-	// your logic here
+	var ss v1.Subspace
+	if err := r.Get(ctx, req.NamespacedName, &ss); err != nil {
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	var sns corev1.Namespace
+	nsn := types.NamespacedName{Name: ss.Name}
+	if err := r.Get(ctx, nsn, &sns); err != nil {
+		if !apierrors.IsNotFound(err) {
+			return ctrl.Result{}, err
+		}
+		// if subnamespace not found create an empty one
+		sns = corev1.Namespace{}
+	}
+
+	if ss.Status.State == v1.Missing {
+		if sns.Name != "" {
+			// if the subnamespace is already created change the state to Ok
+			if _, err := ctrl.CreateOrUpdate(ctx, r, &ss, func() error {
+				ss.Status.State = v1.Ok
+				return nil
+			}); err != nil {
+				return ctrl.Result{}, err
+			}
+		} else {
+			// create the subnamespace and reconcile again
+			sns.Name = ss.Name
+			if _, err := ctrl.CreateOrUpdate(ctx, r, &sns, func() error { return nil }); err != nil {
+				return ctrl.Result{}, err
+			}
+			return ctrl.Result{Requeue: true}, nil
+		}
+	}
+
+	if sns.Name == "" {
+		//TODO:should check if someone did not delete the ns
+		if _, err := ctrl.CreateOrUpdate(ctx, r, &ss, func() error {
+			ss.Status.State = v1.Missing
+			return nil
+		}); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
 
 func (r *SubspaceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&dana794v1.Subspace{}).
+		For(&v1.Subspace{}).
 		Complete(r)
 }
